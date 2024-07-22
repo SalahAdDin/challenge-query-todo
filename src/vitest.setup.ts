34c7dc34b667f841bbdff399fabf0d0a17996e26
José Luis <alagunasalahaddin@live.com>
2024-07-22 17:59:29 -0500

    Initial commit

---
+++ src/vitest.setup.ts
@@ -0,0 +1,13 @@
+import { afterAll, afterEach, beforeAll } from "vitest";
+import "vitest-dom/extend-expect";
+
+import server from "../mocks/server";
+import "./index.css";
+
+// Establish API mocking before all tests.
+
+beforeAll(() => server.listen());
+
+afterEach(() => server.resetHandlers());
+
+afterAll(() => server.close());
