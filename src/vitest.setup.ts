0d7e3d8c6ba78d1a8e162e13f6e9ae5c9bfaaace
Luis Alaguna <alagunasalahaddin@live.com>
2024-07-22 20:58:28 -0500

    chore: removing MSW related tests

--- src/vitest.setup.ts
+++ src/vitest.setup.ts
@@ -1,13 +1,3 @@
-import { afterAll, afterEach, beforeAll } from "vitest";
+// import { afterAll, afterEach, beforeAll } from "vitest";
 import "vitest-dom/extend-expect";
-
-import server from "../mocks/server";
 import "./index.css";
-
-// Establish API mocking before all tests.
-
-beforeAll(() => server.listen());
-
-afterEach(() => server.resetHandlers());
-
-afterAll(() => server.close());
