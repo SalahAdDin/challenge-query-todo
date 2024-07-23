00bd39e417cf463c067fdfd196fc1b0acbc9ae63
Luis Alaguna <alagunasalahaddin@live.com>
2024-07-22 21:19:14 -0500

    fix: :ambulance: adding api key and removing it from env

--- src/infrastructure/api/client.ts
+++ src/infrastructure/api/client.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const { VITE_API_BASE_URL } = import.meta.env;
+const { VITE_API_BASE_URL, VITE_API_KEY } = import.meta.env;
 
 const client = axios.create({
   baseURL: VITE_API_BASE_URL,
   headers: {
-    "X-Api-Key": "",
+    "X-Api-Key": VITE_API_KEY,
   },
 });
 
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,6 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_API_BASE_URL: string;
+  readonly VITE_API_KEY: string;
   readonly VITE_APP_BASE_URL: string;
   readonly VITE_SERVER_HOST: string;
   readonly VITE_SERVER_PORT: string;
