d3091837a812c006ad4513df98631f61f43aeabf
Luis Alaguna <alagunasalahaddin@live.com>
2024-08-08 22:48:28 -0500

    chore: :white_check_mark: adding e2e test

--- tests/App.e2e.test.ts
+++ tests/App.e2e.test.ts
@@ -1,47 +1,93 @@
-import {
-  // expect,
-  test,
-} from "@playwright/test";
+import { expect, test } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  await page.goto("/");
-});
+const todos = [
+  {
+    id: 1,
+    description: "Completed Todo",
+    status: "completed",
+    dueDate: new Date("2024-08-10"),
+    isComplete: true,
+  },
+  {
+    id: 2,
+    description: "Overdue Todo",
+    status: "overdue",
+    dueDate: new Date("2024-08-05"),
+    isComplete: false,
+  },
+  {
+    id: 3,
+    description: "Ongoing Todo",
+    status: "going",
+    dueDate: new Date("2024-08-08"),
+    isComplete: false,
+  },
+];
 
-test.describe("App", () => {
-  /*   test("The user navigates back and forth.", async ({ page }) => {
-    await expect(page.getByText("Users")).toBeVisible();
-
-    await page.getByText("Leanne Graham").click();
-    await expect(page.getByText("Leanne Graham")).toBeVisible();
-
-    await page.locator("a[href='/user/1/album/1']").click();
-    await expect(page.getByText("quidem molestiae enim")).toBeVisible();
-
-    await page.getByRole("button", { name: "Go back" }).click();
-    await expect(page.getByText("Leanne Graham")).toBeVisible();
-
-    await page.getByRole("button", { name: "Go back" }).click();
-    await expect(page.getByText("Users")).toBeVisible();
+test.describe("Todo App", () => {
+  test.beforeEach(async ({ page }) => {
+    await page.goto("/");
   });
 
-  test("Click on fist User, click on first Album, watch all photos.", async ({
+  test("should display spinner while loading", async ({ page }) => {
+    await expect(page.locator("svg[aria-hidden='true']")).toHaveCount(0);
+  });
+
+  test("should display error box when error", async ({ page }) => {
+    await page.route("**/todos", (route) => route.fulfill({ status: 500, body: "Internal Server Error" }));
+
+    await page.reload();
+
+    await page.waitForSelector("[role='alert']");
+
+    await expect(page.locator("[role='alert']")).toContainText(
+      "ErrorRequest failed with status code 500"
+    );
+  });
+
+  test("should display todos sorted by status and due date when fetched", async ({
     page,
   }) => {
-    await expect(page.getByText("Users")).toBeVisible();
-    await page.waitForSelector("table");
-    expect(await page.locator("tr").count()).toBe(11);
+    await page.route("**/todos", (route) => route.fulfill({ status: 200, body: JSON.stringify(todos) }));
+    await page.reload();
 
-    await page.getByText("Leanne Graham").click();
-    await expect(page.getByText("Leanne Graham")).toBeVisible();
-    await expect(page.getByText("Romaguera-Crona")).toBeVisible();
-    await expect(page.getByText("Gwenborough")).toBeVisible();
-    await expect(page.getByText("Sincere@april.biz")).toBeVisible();
-    await page.waitForSelector("a");
-    expect(await page.locator("a").count()).toBe(10);
+    const todoItems = page.locator("li");
 
-    await page.locator("a[href='/user/1/album/1']").click();
-    await expect(page.getByText("quidem molestiae enim")).toBeVisible();
-    await page.waitForSelector("img");
-    expect(await page.locator("img").count()).toBe(50);
-  }); */
+    await expect(todoItems).toHaveCount(3);
+
+    const firstTodo = todoItems.nth(2);
+    const secondTodo = todoItems.nth(1);
+    const thirdTodo = todoItems.nth(0);
+
+    await expect(firstTodo).toContainText(
+      todos[0].description + todos[0].dueDate.toLocaleDateString("en-US")
+    );
+    await expect(secondTodo).toContainText(
+      todos[1].description + todos[1].dueDate.toLocaleDateString("en-US")
+    );
+    await expect(thirdTodo).toContainText(
+      todos[2].description + todos[2].dueDate.toLocaleDateString("en-US")
+    );
+  });
+
+  test("should toggle todo completion status when checking it", async ({
+    page,
+  }) => {
+    await page.route("**/todos", (route) => route.fulfill({ status: 200, body: JSON.stringify([todos[1]]) }));
+
+    await page.reload();
+
+    const checkbox = page.locator("input[type='checkbox']");
+
+    await expect(checkbox).not.toBeChecked();
+
+    await checkbox.check();
+
+    await page.route("**/todos/1", (route) => route.fulfill({
+        status: 200,
+        body: JSON.stringify({ ...todos[0], isComplete: true }),
+      }));
+
+    await expect(checkbox).toBeChecked();
+  });
 });
