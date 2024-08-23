90dc3572b3b17d9d0f4fae4f0f43b9855faae24f
Luis Alaguna <alagunasalahaddin@live.com>
2024-08-23 11:11:27 -0500

    chore: refactoring main tests

--- src/main.test.tsx
+++ src/main.test.tsx
@@ -2,8 +2,6 @@ import React from "react";
 import * as ReactDOM from "react-dom/client";
 
 import { act, render, screen } from "@application/utils/test-utils";
-import App from "@presentation/App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 import reportWebVitals from "./reportWebVitals";
 
@@ -25,10 +23,6 @@ vi.mock("@tanstack/react-query", () => ({
 }));
 
 vi.mock("@tanstack/react-query-devtools", () => ({
-  ReactQueryDevtools: () => <div>ReactQueryDevtools Mock</div>,
-}));
-
-vi.mock("@tanstack/react-query-devtools", () => ({
   ReactQueryDevtools: () => <div>Devtools</div>,
 }));
 
@@ -41,40 +35,22 @@ vi.mock("./reportWebVitals", () => ({
 }));
 
 describe("main.tsx", () => {
-  beforeEach(() => {
-    vi.clearAllMocks();
-    document.body.innerHTML = ""; // Clear body to reset DOM
-  });
+  let originalEnv: NodeJS.ProcessEnv;
 
-  it("renders the application without crashing and calls reportWebVitals", async () => {
-    // const { container } =
+  const setup = async () => {
     render(<div id="root" />);
 
     await act(async () => {
       await import("./main");
     });
-
-    /*
-    const createRootMock = vi.mocked(ReactDOM.createRoot);
-    expect(createRootMock).toHaveBeenCalledTimes(1);
-    expect(createRootMock).toHaveBeenCalledWith(container.firstChild);
-    expect(createRootMock().render).toHaveBeenCalledTimes(1);
-    */
-
-    const webVitalsMock = vi.mocked(reportWebVitals);
-    expect(webVitalsMock).toHaveBeenCalledTimes(1);
-  });
-});
-
-describe("Root component", () => {
-  let originalEnv: NodeJS.ProcessEnv;
+  };
 
   beforeEach(() => {
     originalEnv = { ...process.env };
-    vi.mocked(ReactDOM.createRoot).mockReturnValue({
-      render: vi.fn(),
-      unmount: vi.fn(),
-    });
+
+    vi.clearAllMocks();
+
+    document.body.innerHTML = "";
   });
 
   afterEach(() => {
@@ -82,40 +58,27 @@ describe("Root component", () => {
     vi.clearAllMocks();
   });
 
-  it("should render the App component and ReactQueryDevtools when the development mode is on", () => {
+  it("should render the application and devtools and calls reportWebVitals when the development mode is on", async () => {
     process.env.NODE_ENV = "development";
 
-    const queryClient = new QueryClient();
+    await setup();
 
-    render(
-      <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <App />
-          <React.Suspense fallback={<div>Loading...</div>}>
-            {/* Mocked ReactQueryDevtools component */}
-            <div>ReactQueryDevtools Mock</div>
-          </React.Suspense>
-        </QueryClientProvider>
-      </React.StrictMode>
-    );
+    expect(screen.getByText("App")).toBeInTheDocument();
+    expect(screen.getByText("Devtools")).toBeInTheDocument();
 
-    expect(screen.getByText("ReactQueryDevtools Mock")).toBeInTheDocument();
+    const webVitalsMock = vi.mocked(reportWebVitals);
+    expect(webVitalsMock).toHaveBeenCalledTimes(1);
   });
 
-  it("should not render ReactQueryDevtools when the production mode is on", () => {
+  it("should not render ReactQueryDevtools when the production mode is on", async () => {
     process.env.NODE_ENV = "production";
 
-    const queryClient = new QueryClient();
-    render(
-      <React.StrictMode>
-        <QueryClientProvider client={queryClient}>
-          <App />
-        </QueryClientProvider>
-      </React.StrictMode>
-    );
+    await setup();
 
-    expect(
-      screen.queryByText("ReactQueryDevtools Mock")
-    ).not.toBeInTheDocument();
+    expect(screen.getByText("App")).toBeInTheDocument();
+    expect(screen.queryByText("Devtools")).not.toBeInTheDocument();
+
+    const webVitalsMock = vi.mocked(reportWebVitals);
+    expect(webVitalsMock).toHaveBeenCalledTimes(1);
   });
 });
